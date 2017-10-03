d32d983884fd3de8ace15720401c6526c39c94a4
t-g-j <tgj@TMP.local>
2017-10-03 10:46:52 +0200

    Last changes from me.
    Migratign to my own repository

--- Robotics_Thor/Lab 3/Matrice_rotation/Matrices_rotation/main.cpp
+++ Robotics_Thor/Lab 3/Matrice_rotation/Matrices_rotation/main.cpp
@@ -17,7 +17,7 @@ int main()
     vector<float>rotation;
 
     // z,y,x = 1.5708, 0.261799, 3.14159
-    rotation = {MY_PI,15*(MY_PI/180),MY_PI/2};
+    rotation = {MY_PI,MY_PI,MY_PI/2};
     //rotation = {MY_PI/2, 15*(MY_PI/180), MY_PI};
     //rotation = {MY_PI/2,MY_PI/3,MY_PI/2};
     myRotation.myRot(rotation);
